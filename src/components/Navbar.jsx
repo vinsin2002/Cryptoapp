a61622d4b518bdfdecb7f324eeb668e3e7bc8ac5
vinsin2002 <vinsin2002@gmail.com>
2023-03-05 14:06:43 +0530

    made font changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 function Navbar() {
   return (
     <div className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex flex-col flex-wrap'>
-       <div className=' text-white font-sans text-5xl p-2 ml-8 sm:text-center'>
-         Crypto   
+       <div className=' text-white font-rubik text-5xl p-2 ml-8 sm:text-center'>
+         Crypto.  
         </div>  
-        <ul className='text-white cursor-default flex justify-evenly mt-5 mb-3'>
+        <ul className='text-white font-rubik cursor-default flex justify-evenly mt-5 mb-3'>
             <li className='hover:border-b-2'>
-                watchlist
+                Watchlist
             </li>
             <li className='hover:border-b-2'>
-                portfolio
+                Portfolio
             </li>
             <li className='hover:border-b-2'>
-                account
+                Account
             </li>
         </ul>
     </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      rubik: ["Rubik","sans-serif"]
+    }
   },
   plugins: [],
 }
