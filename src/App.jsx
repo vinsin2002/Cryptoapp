448eaa8cdf9e9d78da3122a262eed3396e63f60b
vinsin2002 <vinsin2002@gmail.com>
2023-03-06 22:15:48 +0530

    responsive issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 import Card from './components/Card'
 function App() {
   return (
-    <div className='h-screen w-screen bg-gray-900'>
+    <div className=' sm:h-screen bg-gray-900'>
     <Navbar/>
     <div className='flex flex-wrap justify-center mt-5'>
     <Card/>
