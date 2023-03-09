2eb7cc61cb5c4075680f86a995ccb0381e838778
vinsin2002 <vinsin2002@gmail.com>
2023-03-09 21:43:36 +0530

    Added  error page

---
+++ src/components/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div className='h-screen bg-gray-900 flex flex-col justify-center items-center'>
+        <h1 className='text-purple-600 text-3xl text-center '>Error 404</h1>
+        <h1 className='text-purple-600 text-5xl text-center '>Page not found!!!!</h1>
+    </div>
+  )
+}
+
+export default Error
--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,14 @@ import App from './App'
 import Portfolio from './Portfolio'
 import './index.css'
 import Account from './components/Account'
-
+import Error from './components/Error'
 ReactDOM.createRoot(document.getElementById('root')).render(
    <BrowserRouter>
     <Routes>
       <Route path="/" element={<App/>} />
       <Route path="/portfolio" element={<Portfolio/>}/>
       <Route path='/account' element={<Account/>}/>
+      <Route path='*' element={<Error/>}/>
     </Routes>
    </BrowserRouter>
   ,
